1b13ff6f3e001108b73ea94d44a1849c9d63b614
koridal <hjpark002@gmail.com>
2025-08-31 21:19:21 +0900

    fix sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,8 +1,8 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './src/sanity/schemaTypes'
 import StudioNavbar from '@/components/StudioNavbar';
+import { schemaTypes } from '@/sanity/schemaTypes';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
