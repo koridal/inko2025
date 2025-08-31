c3040338d45ebe71e721c8e12d862fad757a26fb
koridal <hjpark002@gmail.com>
2025-08-31 18:11:58 +0900

    fix next.config

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,17 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
+  experimental: {
+    // Turbopack Scope Hoisting 기능 비활성화 (현재 알려진 workaround)
+    turbopackScopeHoisting: false,
+  },
 };
 
 export default nextConfig;
+
