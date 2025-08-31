bfc93d4a782a7ca3adb4b097578dbeff1df92d9a
koridal <hjpark002@gmail.com>
2025-08-31 23:39:03 +0900

    fix.schemaTypes

--- src/sanity/schemaTypes.ts
+++ src/sanity/schemaTypes.ts
@@ -1,47 +1,46 @@
-// schemaTypes.ts
-import { defineField, defineType } from "sanity";
+// src/sanity/schemaTypes.ts
+import { defineType, defineField } from "sanity"
 
-const project = defineType({
-  name: "project",
-  title: "Project",
-  type: "document",
-  fields: [
-    defineField({
-      name: "title",
-      title: "Title",
-      type: "string",
-      validation: (rule) => rule.required(),
-    }),
-    defineField({
-      name: "slug",
-      title: "Slug of project article",
-      type: "slug",
-      options: { source: "title" },
-      validation: (rule) => rule.required(),
-    }),
-    defineField({
-      name: "publishedAt",
-      type: "datetime",
-      initialValue: () => new Date().toISOString(),
-      validation: (rule) => rule.required(),
-    }),
-    defineField({
-      name: "titleImage",
-      type: "image",
-      title: "Image of Project",
-    }),
-    defineField({
-      name: "smallDescription",
-      type: "text",
-      title: "Small Description",
-    }),
-    defineField({
-      name: "content",
-      type: "array",
-      of: [{ type: "block" }, { type: "image" }],
-    }),
-  ],
-});
-
-// ✅ 반드시 배열로 export
-export const schemaTypes = [project];
+export const schemaTypes = [
+  defineType({
+    name: 'project',
+    title: 'Project',
+    type: 'document',
+    fields: [
+      defineField({
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+        validation: (rule) => rule.required(),
+      }),
+      defineField({
+        name: 'slug',
+        title: 'Slug of project article',
+        type: 'slug',
+        options: { source: 'title' },
+        validation: (rule) => rule.required(),
+      }),
+      defineField({
+        name: 'publishedAt',
+        type: 'datetime',
+        initialValue: () => new Date().toISOString(),
+        validation: (rule) => rule.required(),
+      }),
+      defineField({
+        name: 'titleImage',
+        type: 'image',
+        title: 'Image of Project',
+      }),
+      defineField({
+        name: 'smallDescription',
+        type: 'text',
+        title: 'Small Description',
+      }),
+      defineField({
+        name: 'content',
+        type: 'array',
+        of: [{ type: 'block' }, { type: 'image' }],
+      }),
+    ],
+  }),
+]
