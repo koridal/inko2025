7b7b1c9f1c9712d94ecfbf907053599dcd1d3467
koridal <hjpark002@gmail.com>
2025-08-31 23:41:28 +0900

    fix.snaity.config

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "sanity";
 export default defineConfig({
   name: "default", // workspace 이름
   title: "My Sanity Studio",
-
   projectId: "your-project-id", // ← Sanity 프로젝트 ID
   dataset: "production",        // ← Dataset (예: production)
 
@@ -13,3 +12,5 @@ export default defineConfig({
     types: schemaTypes, // ✅ 배열로 전달
   },
 });
+
+
